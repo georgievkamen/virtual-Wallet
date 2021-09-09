ca07d2a82cfd163fd53ef4ac0d88c6da3c05f564
kristiyanpd <kristiyanpd02@gmail.com>
2021-09-09 21:14:58 +0300

    Fixed Hibernate config

--- virtual-wallet/src/main/java/com/team9/virtualwallet/config/HibernateConfig.java
+++ virtual-wallet/src/main/java/com/team9/virtualwallet/config/HibernateConfig.java
@@ -28,7 +28,7 @@ public class HibernateConfig {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.team9.deliverit.models");
+        sessionFactory.setPackagesToScan("com.team9.virtualwallet.models");
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
