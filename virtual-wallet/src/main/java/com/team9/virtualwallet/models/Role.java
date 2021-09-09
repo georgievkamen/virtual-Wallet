9864ee2bb0846c9b470963c31c8e0b7cee3ff3fa
kristiyanpd <kristiyanpd02@gmail.com>
2021-09-09 19:24:04 +0300

    Added Role model.

--- virtual-wallet/src/main/java/com/team9/virtualwallet/models/Role.java
+++ virtual-wallet/src/main/java/com/team9/virtualwallet/models/Role.java
@@ -1,4 +1,35 @@
 package com.team9.virtualwallet.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role_name")
+    private String name;
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
