0e05522965f85e1825dcf96242a8ccb4bfec402c
kristiyanpd <kristiyanpd02@gmail.com>
2021-09-09 19:27:08 +0300

    Added BaseRepository.

---
+++ virtual-wallet/src/main/java/com/team9/virtualwallet/repositories/BaseRepositoryImpl.java
@@ -0,0 +1,88 @@
+package com.team9.virtualwallet.repositories;
+
+import org.hibernate.SessionFactory;
+
+public class BaseRepositoryImpl<E> implements BaseRepository<E> {
+
+    private final SessionFactory sessionFactory;
+    private final Class<E> clazz;
+
+    public BaseRepositoryImpl(SessionFactory sessionFactory, Class<E> clazz) {
+        this.sessionFactory = sessionFactory;
+        this.clazz = clazz;
+    }
+
+    @Override
+    public List<E> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<E> query = session.createQuery("from " + clazz.getSimpleName(), clazz);
+            return query.list();
+        }
+    }
+
+    @Override
+    public E getById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            E obj = session.get(clazz, id);
+            if (obj == null) {
+                throw new EntityNotFoundException(clazz.getSimpleName(), id);
+            }
+            return obj;
+        }
+    }
+
+    @Override
+    public <V> E getByField(String fieldName, V value) {
+        List<E> list = getByFieldList(fieldName, value);
+        if (list.isEmpty()) {
+            throw new EntityNotFoundException(clazz.getSimpleName(), fieldName, String.valueOf(value));
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public <V> List<E> getByFieldList(String fieldName, V value) {
+        String query = String.format("from %s where %s = :value", clazz.getSimpleName(), fieldName);
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(query, clazz)
+                    .setParameter("value", value).list();
+        }
+    }
+
+    @Override
+    public <V> List<E> searchByFieldList(String fieldName, V value) {
+        String query = String.format("from %s where %s like :value", clazz.getSimpleName(), fieldName);
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(query, clazz)
+                    .setParameter("value", "%" + value + "%").list();
+        }
+    }
+
+    @Override
+    public void create(E obj) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(obj);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(E obj) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(obj);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        E obj = getById(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.delete(obj);
+            session.getTransaction().commit();
+        }
+    }
+}
---
+++ virtual-wallet/src/main/java/com/team9/virtualwallet/repositories/contracts/BaseRepository.java
@@ -0,0 +1,21 @@
+package com.team9.virtualwallet.repositories.contracts;
+
+import java.util.List;
+
+public interface BaseRepository<E> {
+    List<E> getAll();
+
+    E getById(int id);
+
+    <V> E getByField(String fieldName, V value);
+
+    <V> List<E> getByFieldList(String fieldName, V value);
+
+    <V> List<E> searchByFieldList(String fieldName, V value);
+
+    void create(E obj);
+
+    void update(E obj);
+
+    void delete(int id);
+}
