45e3b1ee9258e2249fa24a42913700160a150c3b
Kamen <nahtata.kg@gmail.com>
2021-09-09 19:23:54 +0300

    implemented Card model

--- virtual-wallet/src/main/java/com/team9/virtualwallet/models/Card.java
+++ virtual-wallet/src/main/java/com/team9/virtualwallet/models/Card.java
@@ -1,4 +1,81 @@
 package com.team9.virtualwallet.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "cards")
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private int id;
+
+    @Column(name = "card_number")
+    private int cardNumber;
+
+    @Column(name = "expiration_date")
+    private Date expirationDate;
+
+    @Column(name = "card_holder")
+    private String cardHolder;
+
+    @Column(name = "cvv")
+    private int cvv;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Card() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCardHolder() {
+        return cardHolder;
+    }
+
+    public void setCardHolder(String cardHolder) {
+        this.cardHolder = cardHolder;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
